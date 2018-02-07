2e0b95f11ce9e20070c0978c5b47dcfab826816d
Nickolas Buchanan <fdnickolas@gmail.com>
2018-02-07 15:12:59 -0500

    changed colors and linked js file

---
+++ assets/train.js
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: "AIzaSyAt86Bgi5iQ9nOkZC24X_KpKgGfvTjad4Y",
+  authDomain: "traintime-efbc8.firebaseapp.com",
+  databaseURL: "https://traintime-efbc8.firebaseio.com",
+  projectId: "traintime-efbc8",
+  storageBucket: "traintime-efbc8.appspot.com",
+  messagingSenderId: "273365151017"
+};
+
+firebase.initializeApp(config);
